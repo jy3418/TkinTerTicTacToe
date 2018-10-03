57d2814182e877d7baf09d81f931870a072c1468
Justin <jy3418@hotmail.com>
2018-10-03 02:56:48 -0700

    Fixed a bug with winning

--- TicTacToe.py
+++ TicTacToe.py
@@ -86,7 +86,7 @@ class Game:
         for x, y, z in [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]:
             if self.tiles[x].marked == self.tiles[y].marked == self.tiles[z].marked == 'X':
                 print('p1 won')
-            elif self.tiles[x].marked == self.tiles[y].marked == self.tiles[z].marked == 'Y':
+            elif self.tiles[x].marked == self.tiles[y].marked == self.tiles[z].marked == 'O':
                 print('p2 won')
 
 
